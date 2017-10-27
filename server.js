c8d4788aca6e6448636bc87702647369f1769f5a
justin deguzman <justindeguzman@justins-mbp.attlocal.net>
2017-10-26 23:07:13 -0700

    firstPush

---
+++ server.js
@@ -0,0 +1,15 @@
+// ===============================================================================
+// DEPENDENCIES
+// ===============================================================================
+
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+// ===============================================================================
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+// ===============================================================================
+var path = require("path");
+
